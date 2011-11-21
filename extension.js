6b5d676e78aeeaa5330438c4b090b392ec3a3f85
Marco Barisione <marco@barisione.org>
2011-11-21 08:51:13 +0000

    Hook into the shell to get notified when the message count changes

--- extension.js
+++ extension.js
@@ -16,7 +16,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+const Lang = imports.lang;
 const Main = imports.ui.main;
+const MessageTray = imports.ui.messageTray;
 const St = imports.gi.St;
 
 let label;
@@ -32,22 +34,37 @@ MessageLabel.prototype = {
         this.actor = new St.Button({name: 'messageButton'});
         this.actor.set_child(this.countLabel);
 
-        this._updateText();
+        this.updateCount();
     },
 
-    _updateText: function() {
+    updateCount: function() {
         this.countLabel.set_text('0');
     }
 };
 
+function customSetCount(count, visible) {
+    let fallbackSetCount = Lang.bind(this, originalSetCount);
+    fallbackSetCount(count, visible);
+
+    label.updateCount();
+}
+
+let originalSetCount;
+
 function init() {
+    originalSetCount = MessageTray.Source.prototype._setCount;
+
     label = new MessageLabel();
 }
 
 function enable() {
+    MessageTray.Source.prototype._setCount = customSetCount;
+
     Main.panel._rightBox.insert_actor(label.actor, 0);
 }
 
 function disable() {
+    MessageTray.Source.prototype._setCount = originalSetCount;
+
     Main.panel._rightBox.remove_actor(label);
 }
